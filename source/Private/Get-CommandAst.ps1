bf5489cd083d7a05e3acebedac99de8f4767e068
Johan Ljunggren <johan.ljunggren@shamn.se>
2024-06-27 11:13:11 +0200

    Fix Get-CommandAst

--- source/Private/Get-CommandAst.ps1
+++ source/Private/Get-CommandAst.ps1
@@ -1,4 +1,33 @@
-# TODO: Add comment-based help
+<#
+    .SYNOPSIS
+        Retrieves the abstract syntax trees (AST's) of a PowerShell command.
+
+    .DESCRIPTION
+        The Get-CommandAst function retrieves the abstract syntax tree (AST) of a
+        PowerShell command.  The AST represents the structure of the command and
+        can be used for further analysis or manipulation.
+
+    .PARAMETER Ast
+        Specifies the AST of a script block.
+
+    .PARAMETER CommandName
+        Specifies the PowerShell command for which to retrieve the AST.
+
+    .EXAMPLE
+        Get-CommandAst -Command 'Should'
+
+        This example retrieves the AST of the 'Should' command.
+
+    .INPUTS
+        System.Management.Automation.Language.Ast.
+
+        The AST of a script block.
+
+    .OUTPUTS
+        System.Management.Automation.Language.CommandAst[]
+
+        The function returns the abstract syntax trees (AST's) of the specified PowerShell command.
+#>
 function Get-CommandAst
 {
     [CmdletBinding()]
@@ -14,10 +43,10 @@ function Get-CommandAst
         $CommandName
     )
 
-    $shouldAsts = $Ast.FindAll({
+    $commandAsts = $Ast.FindAll({
         param($node)
         return $node -is [System.Management.Automation.Language.CommandAst] -and $node.GetCommandName() -eq $CommandName
     }, $true)
 
-    return $shouldAsts
+    return $commandAsts
 }
