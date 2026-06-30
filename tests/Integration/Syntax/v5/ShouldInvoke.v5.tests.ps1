561931d2c37ab3d1404f4291a5065b22f5fa4058
Johan Ljunggren <johan.ljunggren@shamn.se>
2026-06-30 17:35:33 +0200

    Fix integ test

--- tests/Integration/Syntax/v5/ShouldInvoke.v5.tests.ps1
+++ tests/Integration/Syntax/v5/ShouldInvoke.v5.tests.ps1
@@ -106,15 +106,13 @@ Describe 'Should -Invoke' {
     }
 
     It 'Should convert `Should -Invoke` even if it has another `Should` in its extent' {
-        InModuleScope -Parameters $_ -ScriptBlock {
-            1..3 | ForEach-Object { Get-Something -Path 'test.txt' }
+        1..3 | ForEach-Object { Get-Something -Path 'test.txt' }
 
-            Should -Invoke -CommandName TestCommand -ParameterFilter {
-                'a' | Should -MatchExactly 'a'
+        Should -Invoke -CommandName TestCommand -ParameterFilter {
+            'a' | Should -MatchExactly 'a'
 
-                # Return $true if the assert above does not throw.
-                $true
-            } -Exactly -Times 1 -Scope It
-        }
+            # Return $true if the assert above does not throw.
+            $true
+        } -Exactly -Times 1 -Scope It
     }
 }
